6b26177d77bc800ef1d6d90f1050382e0a590c6d
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-14 23:06:00 +0700

    styling post

--- pages/[tag]/[postUrl].js
+++ pages/[tag]/[postUrl].js
@@ -22,7 +22,7 @@ export default function Page() {
 function Article({ content }) {
   return (
     <>
-      <section className="container py-4 max-w-4xl md:mx-auto">
+      <section className="container py-4 max-w-4xl md:mx-auto mb-24">
         <div className="post px-8">
           {/* <h1 className="font-bold text-4xl text-center pb-5">This is title</h1> */}
           <div className="content text-gray-600 text-lg flex flex-col gap-4">
