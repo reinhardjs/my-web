20cd4df379bd63f23cb10f759fad435ca47c3f3a
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-14 23:06:37 +0700

    render empty string instead of "No Description"

--- pages/[tag]/[postUrl].js
+++ pages/[tag]/[postUrl].js
@@ -26,7 +26,7 @@ function Article({ content }) {
         <div className="post px-8">
           {/* <h1 className="font-bold text-4xl text-center pb-5">This is title</h1> */}
           <div className="content text-gray-600 text-lg flex flex-col gap-4">
-            <ReactMarkdown>{content || "No Description"}</ReactMarkdown>
+            <ReactMarkdown>{content || ""}</ReactMarkdown>
           </div>
         </div>
       </section>
