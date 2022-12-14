a8ab33b5360d559d8c0039618f3dc28732d24ec4
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-15 00:20:05 +0700

    uses react-syntax-highlighter

--- pages/[tag]/[postUrl].js
+++ pages/[tag]/[postUrl].js
@@ -1,32 +1,100 @@
 import ReactMarkdown from "react-markdown";
 import { useRouter } from "next/router";
 
+import rangeParser from "parse-numeric-range";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import { prism } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
+import java from "react-syntax-highlighter/dist/cjs/languages/prism/java";
+import kotlin from "react-syntax-highlighter/dist/cjs/languages/prism/kotlin";
+import javascript from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import go from "react-syntax-highlighter/dist/cjs/languages/prism/go";
+import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
+import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
+import scss from "react-syntax-highlighter/dist/cjs/languages/prism/scss";
+import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
+import markdown from "react-syntax-highlighter/dist/cjs/languages/prism/markdown";
+import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
+
 import ErrorComponent from "../../components/_child/error";
 import Spinner from "../../components/_child/spinner";
 import fetcher from "../../lib/fetcher";
-// import { SWRConfig } from "swr";
+
+SyntaxHighlighter.registerLanguage("java", java);
+SyntaxHighlighter.registerLanguage("kotlin", kotlin);
+SyntaxHighlighter.registerLanguage("javascript", javascript);
+SyntaxHighlighter.registerLanguage("go", go);
+SyntaxHighlighter.registerLanguage("tsx", tsx);
+SyntaxHighlighter.registerLanguage("typescript", typescript);
+SyntaxHighlighter.registerLanguage("scss", scss);
+SyntaxHighlighter.registerLanguage("bash", bash);
+SyntaxHighlighter.registerLanguage("markdown", markdown);
+SyntaxHighlighter.registerLanguage("json", json);
 
 export default function Page() {
   const router = useRouter();
   const { postUrl, tag } = router.query;
-  const { data, isLoading, isError } = fetcher(`posts/${postUrl}`);
 
-  console.log(data);
+  const { data, isLoading, isError } = fetcher(`posts/${postUrl}`);
 
   if (isLoading) return <Spinner></Spinner>;
   if (isError) return <ErrorComponent></ErrorComponent>;
+
+  if (!data || !tag) return undefined;
 
   return <Article {...data.data}></Article>;
 }
 
 function Article({ content }) {
+  const syntaxTheme = prism;
+
+  const MarkdownComponents = {
+    code({ node, inline, className, ...props }) {
+      const match = /language-(\w+)/.exec(className || "");
+      const hasMeta = node?.data?.meta;
+
+      const applyHighlights = (applyHighlights) => {
+        if (hasMeta) {
+          const RE = /{([\d,-]+)}/;
+          const metadata = node.data.meta?.replace(/\s/g, "");
+          const strlineNumbers = RE?.test(metadata)
+            ? RE?.exec(metadata)[1]
+            : "0";
+          const highlightLines = rangeParser(strlineNumbers);
+          const highlight = highlightLines;
+          const data = highlight.includes(applyHighlights) ? "highlight" : null;
+          return { data };
+        } else {
+          return {};
+        }
+      };
+
+      return match ? (
+        <SyntaxHighlighter
+          style={syntaxTheme}
+          language={"bash"}
+          PreTag="div"
+          className="codeStyle"
+          showLineNumbers={false}
+          wrapLines={hasMeta ? true : false}
+          useInlineStyles={true}
+          lineProps={applyHighlights}
+          {...props}
+        />
+      ) : (
+        <code className={className} {...props} />
+      );
+    },
+  };
   return (
     <>
       <section className="container py-4 max-w-4xl md:mx-auto mb-24">
         <div className="post px-8">
           {/* <h1 className="font-bold text-4xl text-center pb-5">This is title</h1> */}
           <div className="content text-gray-600 text-lg flex flex-col gap-4">
-            <ReactMarkdown>{content || ""}</ReactMarkdown>
+            <ReactMarkdown components={MarkdownComponents}>
+              {content || ""}
+            </ReactMarkdown>
           </div>
         </div>
       </section>
