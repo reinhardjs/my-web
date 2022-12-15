a3add81200ffc0c090574d2c02ddccddc9f594d5
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-15 10:05:00 +0700

    styling landing page

--- pages/index.js
+++ pages/index.js
@@ -10,28 +10,36 @@ export default function Home() {
   return (
     <>
       <div className={styles.main}>
-        <h1 className="text-4xl md:text-5xl font-black text-center">
+        <h1 className="text-4xl md:text-5xl font-black text-center pt-20 pb-12">
           Hi! {"I'm"} Reinhard Jonathan Silalahi
         </h1>
 
         <div className="max-w-3xl">
-          <div className="text-center text-lg md:text-xl mt-14">
-            <p className="leading-10">
+          <div className="text-center text-lg md:text-xl mt-0 pb-8">
+            <p className="leading-10 pb-4">
               Welcome to my personal blog/journal site. Here you can find topics
               related to backend, kubernetes, android, and so on.
-              <br />
-              <br />
-              Happy learning!
-              <br />
-              <br />
             </p>
+            <p className="leading-10">Happy learning!</p>
           </div>
         </div>
 
         <div className={styles.grid}>
+          <Link href="/learn-backend">
+            <a className={styles.card}>
+              <h2 className="text-lg font-bold mb-2 mt-0">
+                Learn Backend &rarr;
+              </h2>
+              <p className="text-md">
+                Here is my journey begins (again), you can find all of my
+                backend learning journey here
+              </p>
+            </a>
+          </Link>
+
           <Link href="/learn-kubernetes">
             <a className={styles.card}>
-              <h2 className="text-lg font-bold mb-2">
+              <h2 className="text-lg font-bold mb-2 mt-0">
                 Learn Kubernetes &rarr;
               </h2>
               <p className="text-md">
@@ -40,25 +48,17 @@ export default function Home() {
             </a>
           </Link>
 
-          {/* <Link href="/learn-backend">
-            <a className={styles.card}>
-              <h2 className="text-lg font-bold mb-2">Learn Backend &rarr;</h2>
-              <p className="text-md">
-                Here is my journey begins (again), you can find all of my
-                backend learning journey here
-              </p>
-            </a>
-          </Link>
-
           <Link href="/learn-android">
             <a className={styles.card}>
-              <h2 className="text-lg font-bold mb-2">Learn Android &rarr;</h2>
+              <h2 className="text-lg font-bold mb-2 mt-0">
+                Learn Android &rarr;
+              </h2>
               <p className="text-md">
                 Maybe you can find the reason here why i left Android Dev. But
                 not so sure, I am still on it as a hobby.
               </p>
             </a>
-          </Link> */}
+          </Link>
         </div>
       </div>
       {/* <Section1></Section1>
